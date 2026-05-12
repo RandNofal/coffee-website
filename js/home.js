2c4730b7431b3c01603e8b432bdebf2fb926d92d
RandNofal <rand.nofal453@gmail.com>
2026-05-12 12:21:20 +0300

    overlay

--- js/home.js
+++ js/home.js
@@ -1,18 +1,28 @@
 const menu = document.querySelector(".links");
 const openBtn = document.getElementById("mobile-menu");
 const closeBtn = document.getElementById("close");
+const overlay = document.getElementById("overlay");
 
 openBtn.onclick = function () {
     menu.classList.add("active");
+    overlay.style.display = "block";
 };
 
 closeBtn.onclick = function () {
     menu.classList.remove("active");
+    overlay.style.display = "none";
 };
+
 const links = document.querySelectorAll(".links a");
 
 links.forEach(link => {
     link.onclick = function () {
         menu.classList.remove("active");
+        overlay.style.display = "none";
     };
 });
+
+overlay.onclick = function () {
+    menu.classList.remove("active");
+    overlay.style.display = "none";
+};
